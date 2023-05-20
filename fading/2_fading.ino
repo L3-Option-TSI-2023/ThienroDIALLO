8534d141015019d95698e58639f9a5911ffb717b
sereDIALLO <127400655+sereDIALLO@users.noreply.github.com>
2023-05-20 14:00:01 +0200

    Update 2_fading.ino

--- fading/2_fading.ino
+++ fading/2_fading.ino
@@ -2,15 +2,17 @@ const int led1 = 2;
 const int temps = 10; //Temps de pause pour chaque etat
 const int minLum = 20; // luminosité minimale de la led
 const int maxLum = 255; // luminosité miximale de la led
-const float tensionSeuil = 2.0;
+const float tensionSeuil = 2.0;//tension de seuil de la led
 void setup() {
   // put your setup code here, to run once:
   pinMode(led1, OUTPUT);
 }
 
 void loop() {
+  // on lit la tension sur le potentiomètre
   int tensionPotentio = analogRead(1);
-  int voltage = (255*tensionPotentio) / 1023 ;
+  // Nous convertissons la valeur du potentiomètre, qui varie de 0 à 1023, en une valeur qui varie de 0 à 255.
+  int voltage = (255*tensionPotentio) / 1023 ; // Nous aurions également pu utiliser la fonction map d'Arduino pour effectuer une conversion plus rapide.
   while(voltage >= tensionSeuil)
   {
     for(int i = 20; i <= 255; ++i)
